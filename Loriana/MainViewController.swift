747159abbdb462ca1ba01a206edc9d57346da5c3
Robert Keffury <bobby.keffury@me.com>
2020-03-21 00:36:42 -0700

    Cleaned Up.

--- Loriana/MainViewController.swift
+++ Loriana/MainViewController.swift
@@ -14,29 +14,15 @@ class MainViewController: UIViewController {
     
     //MARK: - Outlets
     
-    @IBOutlet weak var tourButton: UIButton!
-    @IBOutlet weak var scrollView: UIScrollView!
-    
     //MARK: - Views
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        setUpSubviews()
-        scrollView.delegate = self
     }
     
     //MARK: - Methods
     
-    func setUpSubviews() {
-        tourButton.layer.cornerRadius = 15.0
-    }
-    
     //MARK: - Actions
 
 
-}
-
-extension MainViewController: UIScrollViewDelegate {
-    
 }
