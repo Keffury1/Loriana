0389142292f43af569eb973f96e153a211b2914c
Robert Keffury <bobby.keffury@me.com>
2020-03-21 00:35:01 -0700

    Added Rest Of Storyboard.

--- Loriana/MainViewController.swift
+++ Loriana/MainViewController.swift
@@ -10,19 +10,33 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    //MARK: - Properties
+    
+    //MARK: - Outlets
+    
     @IBOutlet weak var tourButton: UIButton!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    //MARK: - Views
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setUpSubviews()
+        scrollView.delegate = self
     }
     
+    //MARK: - Methods
     
     func setUpSubviews() {
         tourButton.layer.cornerRadius = 15.0
     }
     
+    //MARK: - Actions
 
 
+}
+
+extension MainViewController: UIScrollViewDelegate {
+    
 }
