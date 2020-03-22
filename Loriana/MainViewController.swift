e3912a2cc8e85ae4e07c28414511b57a7805350c
Robert Keffury <bobby.keffury@me.com>
2020-03-21 18:55:25 -0700

    Fixed Display Issues.

--- Loriana/MainViewController.swift
+++ Loriana/MainViewController.swift
@@ -14,14 +14,26 @@ class MainViewController: UIViewController {
     
     //MARK: - Outlets
     
+    @IBOutlet weak var bookTourButton: UIButton!
+    @IBOutlet weak var weddingsTextField: UITextView!
+    @IBOutlet weak var corporateTextField: UITextView!
+    
     //MARK: - Views
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupSubviews()
     }
     
     //MARK: - Methods
     
+    private func setupSubviews() {
+        bookTourButton.layer.cornerRadius = 10.0
+        
+        weddingsTextField.backgroundColor = .white
+        corporateTextField.backgroundColor = .white
+    }
     //MARK: - Actions
 
 
