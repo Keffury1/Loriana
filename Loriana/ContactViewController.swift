b6373ff41eb90fcfb30b73b5dcb66a89311e944d
Robert Keffury <bobby.keffury@me.com>
2020-03-22 15:23:05 -0700

    Added VC Files and Wired them up to VCs in storyboard.

---
+++ Loriana/ContactViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ContactViewController.swift
+//  Loriana
+//
+//  Created by Bobby Keffury on 3/22/20.
+//  Copyright © 2020 Bobby Keffury. All rights reserved.
+//
+
+import UIKit
+
+class ContactViewController: UIViewController {
+
+    //MARK: - Properties
+    
+    //MARK: - Outlets
+    
+    //MARK: - Views
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    //MARK: - Methods
+    
+    //MARK: - Actions
+    
+    //MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+
+
+}
---
+++ Loriana/PlannersViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PlannersViewController.swift
+//  Loriana
+//
+//  Created by Bobby Keffury on 3/22/20.
+//  Copyright © 2020 Bobby Keffury. All rights reserved.
+//
+
+import UIKit
+
+class PlannersViewController: UIViewController {
+
+    //MARK: - Properties
+    
+    //MARK: - Outlets
+    
+    //MARK: - Views
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    //MARK: - Methods
+    
+    //MARK: - Actions
+    
+    //MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+
+
+}
