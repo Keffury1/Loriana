bdbf91de8fbfa1aac2ea3528778af48fec4196ff
Robert Keffury <bobby.keffury@me.com>
2020-03-22 08:42:33 -0700

    Added View & Buttons inside the view.

--- Loriana/MainViewController.swift
+++ Loriana/MainViewController.swift
@@ -17,6 +17,7 @@ class MainViewController: UIViewController {
     @IBOutlet weak var bookTourButton: UIButton!
     @IBOutlet weak var weddingsTextField: UITextView!
     @IBOutlet weak var corporateTextField: UITextView!
+    @IBOutlet weak var menuView: UIView!
     
     //MARK: - Views
     
@@ -33,7 +34,33 @@ class MainViewController: UIViewController {
         
         weddingsTextField.backgroundColor = .white
         corporateTextField.backgroundColor = .white
+        
+        let screenRect = UIScreen.main.bounds
+        let screenWidth = screenRect.size.width
+        let screenHeight = screenRect.size.height
+        
+        let viewRect = CGRect(x: 0, y: 0, width: screenWidth / 2, height: screenHeight)
+        
+        menuView.frame = viewRect
+        menuView.layer.backgroundColor = CGColor(srgbRed: 204/255, green: 155/255, blue: 135/255, alpha: 1)
+        menuView.alpha = 0
+        
+        self.view.addSubview(menuView)
     }
+    
+    func menuOn() {
+        UIView.animate(withDuration: 1.0, animations: {
+            self.menuView.alpha = 1
+        })
+    }
+    
+    func menuOff() {
+        UIView.animate(withDuration: 1.0, animations: {
+            self.menuView.alpha = 0
+        })
+    }
+    
+    
     //MARK: - Actions
 
 
